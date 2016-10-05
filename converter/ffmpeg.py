3acd9fb355a8f97d082582e93c8f3e34bd411c36
magicknight <liangzhihua@gmail.com>
2016-10-04 22:17:42 -0500

    fix condec problem

--- converter/ffmpeg.py
+++ converter/ffmpeg.py
@@ -450,7 +450,7 @@ class FFMpeg(object):
             if not ret:
                 break
 
-            ret = ret.decode(console_encoding)
+            # ret = ret.decode(console_encoding)
             total_output += ret
             buf += ret
             if '\r' in buf:
